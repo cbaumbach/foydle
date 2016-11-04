9f4664628c4670250c86107292e244541f6bfdbc
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-11-04 09:21:52 +0100

    Clean up test case by using default arguments.

--- R/testing.R
+++ R/testing.R
@@ -1,4 +1,4 @@
-create_data <- function(x, y, z) {
+create_data <- function(x = 2, y = 3, z = 4) {
     list(x = create_matrix(paste0("x", seq_len(x))),
         y = create_matrix(paste0("y", seq_len(y))),
         z = create_matrix(paste0("z", seq_len(z))))
--- tests/testthat/test-foydle.R
+++ tests/testthat/test-foydle.R
@@ -3,7 +3,7 @@ source("setup.R")
 context("foydle")
 
 test_that("happy path", {
-    mat <- create_data(x = 2, y = 3, z = 4)
+    mat <- create_data()
     result <- read.delim(as.is = TRUE,
         foydle(mat$x, mat$y, mat$z, output_file = "data/out.txt"))
     expect_equal(result, foydle_lm(mat$x, mat$y, mat$z))
