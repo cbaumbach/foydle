4a00e9dd99933fe88de4780f1e69e1e8b81acbca
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-11-04 09:16:07 +0100

    Refactor test case.

---
+++ R/testing.R
@@ -0,0 +1,11 @@
+create_data <- function(x, y, z) {
+    list(x = create_matrix(paste0("x", seq_len(x))),
+        y = create_matrix(paste0("y", seq_len(y))),
+        z = create_matrix(paste0("z", seq_len(z))))
+}
+
+create_matrix <- function(colnames, nobs = 20) {
+    matrix(rnorm(nobs * length(colnames)),
+        ncol = length(colnames),
+        dimnames = list(NULL, colnames))
+}
--- tests/testthat/test-foydle.R
+++ tests/testthat/test-foydle.R
@@ -4,12 +4,8 @@ dir.create("data", showWarnings = FALSE)
 context("foydle")
 
 test_that("foydle", {
-    N <- 20
-    xmat <- matrix(rnorm(N * 2), ncol = 2, dimnames = list(NULL, c("x1", "x2")))
-    ymat <- matrix(rnorm(N * 3), ncol = 3, dimnames = list(NULL, c("y1", "y2", "y3")))
-    zmat <- matrix(rnorm(N * 4), ncol = 4, dimnames = list(NULL, c("z1", "z2", "z3", "z4")))
-
-    result <- read.delim(foydle(xmat, ymat, zmat, output_file = "data/out.txt"), as.is = TRUE)
-
-    expect_equal(result, foydle_lm(xmat, ymat, zmat))
+    mat <- create_data(x = 2, y = 3, z = 4)
+    result <- read.delim(as.is = TRUE,
+        foydle(mat$x, mat$y, mat$z, output_file = "data/out.txt"))
+    expect_equal(result, foydle_lm(mat$x, mat$y, mat$z))
 })
