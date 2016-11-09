32d254003a8af43a5af282850dbff405cb43f1eb
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-11-09 15:40:08 +0100

    Add "nobs" argument to create_data function.

--- R/testing.R
+++ R/testing.R
@@ -1,10 +1,10 @@
-create_data <- function(x = 2, y = 3, z = 4) {
-    list(x = create_matrix(paste0("x", seq_len(x))),
-        y = create_matrix(paste0("y", seq_len(y))),
-        z = create_matrix(paste0("z", seq_len(z))))
+create_data <- function(x = 2, y = 3, z = 4, nobs = 20) {
+    list(x = create_matrix(paste0("x", seq_len(x)), nobs),
+        y = create_matrix(paste0("y", seq_len(y)), nobs),
+        z = create_matrix(paste0("z", seq_len(z)), nobs))
 }
 
-create_matrix <- function(colnames, nobs = 20) {
+create_matrix <- function(colnames, nobs) {
     matrix(stats::rnorm(nobs * length(colnames)),
         ncol = length(colnames),
         dimnames = list(NULL, colnames))
